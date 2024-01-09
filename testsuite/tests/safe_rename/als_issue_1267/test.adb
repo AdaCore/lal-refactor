88de493c8b791c94172fdf5f3bb036285d91f622
Joao Azevedo <azevedo@adacore.com>
2024-01-09 14:39:42 +0000

    Replace P_Defining_Name by loop over P_Defining_Names
    Only done in places where the declaration can have more than one
    Defining_Name, for instance, object declarations.

---
+++ testsuite/tests/safe_rename/als_issue_1267/test.adb
@@ -0,0 +1,15 @@
+procedure Test is
+   A, B : Integer;
+begin
+   declare
+      C, D : Integer;
+   begin
+      declare
+         E, F : Integer;
+      begin
+         B := 1;
+   null;
+      end;
+   end;
+   null;
+end Test;
