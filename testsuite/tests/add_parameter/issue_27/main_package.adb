78714565d63a819b1d1808be4898b0747581eb67
Joao Azevedo <azevedo@adacore.com>
2024-10-23 11:51:10 +0000

    Fix 'Add Parameter' on functions without parameters

---
+++ testsuite/tests/add_parameter/issue_27/main_package.adb
@@ -0,0 +1,5 @@
+package body Main_Package is
+
+   function Get_Other_File return Integer is null;
+
+end Main_Package;
---
+++ testsuite/tests/add_parameter/issue_27/test.sh
@@ -0,0 +1,1 @@
+add_parameter -P default.gpr -S main_package.ads -L 3 -R 6 -N 'Success : out Boolean'
